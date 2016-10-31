dfe5dcbbaf9f3a81e7c8087cb0fbbcfabc8dc195
chris-daynes <chrisdaynes@gmail.com>
2016-10-31 21:22:53 +1300

    Repaired the malfunctioning update function and returns re configured deleteStudent function

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ function getStudentById (id, callback) {
 // }
 
 function updateStudent (student, callback) {
-  return knex('students').update({name: student.name, cohort: student.cohort}).asCallback(callback)
+  return knex('students').where('id', '=', student.id).update({name: student.name, cohort: student.cohort}).asCallback(callback)
 }
 
 // Update
@@ -32,10 +32,14 @@ function updateStudent (student, callback) {
 //   return knex.raw(`UPDATE students SET name = '${student.name}', cohort = '${student.cohort}' WHERE id = '${student.id}'`).asCallback(callback)
 // }
 
-// Delete
 function deleteStudentById (id, callback) {
-  return knex.raw(`DELETE FROM students WHERE id = '${id}'`).asCallback(callback)
+  return knex('students').where('id', id).delete().asCallback(callback)
 }
+
+// Delete
+// function deleteStudentById (id, callback) {
+//   return knex.raw(`DELETE FROM students WHERE id = '${id}'`).asCallback(callback)
+// }
 
 //
 // createStudent({name: 'Pouramua Kupenga', cohort: 'Piwakawaka-2016', }, function( err, res) {
@@ -48,15 +52,15 @@ getStudentById('6', function (err, res) {
   console.log(res)
 })
 
-updateStudent({id: 11, name: 'Chris Daynes', cohort: 'Piwakawaka-2016'}, function (err, res) {
+updateStudent({id: 7, name: 'Chris Daynes', cohort: 'Piwakawaka-2016'}, function (err, res) {
   if (err) return console.log(err)
   console.log(res)
 })
 
-// deleteStudentById('8', function(err, res) {
-//   if (err) return console.log(err)
-//   console.log(res);
-// })
+deleteStudentById('6', function (err, res) {
+  if (err) return console.log(err)
+  console.log(res);
+})
 
 getAllStudents(function (err, res) {
   if (err) return console.log(err)
