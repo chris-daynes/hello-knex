1ad3fee238f6d86558660e2fc97d1c95283f4286
chris-daynes <chrisdaynes@gmail.com>
2016-10-31 21:02:26 +1300

    changed some formatiing but nothing major

--- index.js
+++ index.js
@@ -1,35 +1,34 @@
 var Knex = require('knex')
-var knexConfig = require('./knexfile.js').development;
+var knexConfig = require('./knexfile.js').development
 var knex = Knex(knexConfig)
 
-//Create
-function createStudent(student, callback) {
-  
+// Create
+function createStudent (student, callback) {
+
 }
 
-//Read
-function getAllStudents(callback) {
+// Read
+function getAllStudents (callback) {
   return knex.select().table('students').asCallback(callback)
 }
 
-//Read
-function getStudentById(id, callback) {
-  
+// Read
+function getStudentById (id, callback) {
+
 }
 
-//Update
+// Update
 function updateStudent(student, callback) {
-  
+
 }
 
-//Delete
+// Delete
 function deleteStudentById(id, callback) {
   
 }
 
-
-//Let's try out the getAllStudents function.
+// Let's try out the getAllStudents function.
 getAllStudents(function(err, res) {
-  if(err) return  console.log(err)
+  if (err) return  console.log(err)
   console.log(res)
 })
