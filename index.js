0ea6c108ed6d0c21c20f9a186efa2065fc9adcf5
chris-daynes <chrisdaynes@gmail.com>
2016-10-31 21:11:51 +1300

    Reconfigured update student function

--- index.js
+++ index.js
@@ -1,36 +1,40 @@
 var Knex = require('knex')
-var knexConfig = require('./knexfile.js').development;
+var knexConfig = require('./knexfile.js').development
 var knex = Knex(knexConfig)
 
-//Create
-function createStudent(student, callback) {
+// Create
+function createStudent (student, callback) {
   return knex('students').insert(student).asCallback(callback)
 }
 // function createStudent(student, callback) {
 //   return knex.raw(`INSERT INTO students (name, cohort) VALUES ('${student.name}', '${student.cohort}')`).asCallback(callback)
 // }
 
-//Read
-function getAllStudents(callback) {
+// Read
+function getAllStudents (callback) {
   return knex.select().table('students').asCallback(callback)
 }
 
-//Read
-function getStudentById(id, callback) {
+// Read
+function getStudentById (id, callback) {
   return knex('students').where('id', id).asCallback(callback)
 }
 // function getStudentById(id, callback) {
 //   return knex.raw(`SELECT * FROM students WHERE id = '${id}'`).asCallback(callback)
 // }
 
-//Update
-function updateStudent(student, callback) {
-  return knex.raw(`UPDATE students SET name = '${student.name}', cohort = '${student.cohort}' WHERE id = '${student.id}'`).asCallback(callback)
+function updateStudent (student, callback) {
+  return knex('students').update({name: student.name, cohort: student.cohort}).asCallback(callback)
 }
 
-//Delete
-function deleteStudentById(id, callback) {
- return knex.raw(`DELETE FROM students WHERE id = '${id}'`).asCallback(callback)
+// Update
+// function updateStudent (student, callback) {
+//   return knex.raw(`UPDATE students SET name = '${student.name}', cohort = '${student.cohort}' WHERE id = '${student.id}'`).asCallback(callback)
+// }
+
+// Delete
+function deleteStudentById (id, callback) {
+  return knex.raw(`DELETE FROM students WHERE id = '${id}'`).asCallback(callback)
 }
 
 //
@@ -41,12 +45,12 @@ function deleteStudentById(id, callback) {
 
 getStudentById('6', function (err, res) {
   if (err) return console.log(err)
-  console.log(res);
+  console.log(res)
 })
 
-updateStudent({id: 6, name: 'Chris Daynes', cohort: 'Piwakawaka-2016'}, function (err, res) {
+updateStudent({id: 11, name: 'Chris Daynes', cohort: 'Piwakawaka-2016'}, function (err, res) {
   if (err) return console.log(err)
-  console.log(res);
+  console.log(res)
 })
 
 // deleteStudentById('8', function(err, res) {
@@ -54,7 +58,7 @@ updateStudent({id: 6, name: 'Chris Daynes', cohort: 'Piwakawaka-2016'}, function
 //   console.log(res);
 // })
 
-getAllStudents(function(err, res) {
-  if(err) return  console.log(err)
+getAllStudents(function (err, res) {
+  if (err) return console.log(err)
   console.log(res)
 })
